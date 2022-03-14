31839c1eb376df0a342966dfa26fc5610b47e190
cpWhitecat <19803cp@gmial.com>
2022-03-14 14:13:56 +0800

    关于约束函数

--- Function.js
+++ Function.js
@@ -92,3 +92,26 @@ zconsole.log(cpp);
                   console.log
                 })
               )()
+              
+              
+              //调试成功 git使用起来是真的简单，快速入门
+              
+              
+              function out(){
+                var name = 20;
+                var x = function in() {
+                    var  = name
+                }
+              }
+              // lambda 匿名函数 希腊字母像中文的入
+              
+              
+              
+              // Function.prototype.bind()
+              // bind() 方法创建一个新的函数，
+              // 在 bind() 被调用时，这个新函数的 this 被指定为 bind() 的第一个参数，
+              // 而其余参数将作为新函数的参数，供调用时使用。
+              
+              
+              
+              
