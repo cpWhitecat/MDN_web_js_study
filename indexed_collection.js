98f74e25dff1f7b6fb78cf0aad0fe73f8e6379b9
cpWhitecat <19803cp@gmial.com>
2022-03-17 09:38:53 +0800

    main

--- indexed_collection.js
+++ indexed_collection.js
@@ -3,3 +3,44 @@
 // 只是它的元素罢了
 
 // 所以第一个arr的数组长度是1 第二个arr的数组长度是4
+
+var arr = new Array(arrayLength);
+var arr = Array(arrayLength);
+
+// 这样有同样的效果
+var arr = [];
+arr.length = arrayLength;
+
+
+
+
+
+
+// distirbution//分配，分布，分发
+// sincere //真诚的真挚的
+// mood //心情
+// senator // 参议员  senate 是参议院的意思
+// lid //男孩 ，小伙子 指男性
+// describe //形容 描写 画出（图像等）
+// equip //v 装备 ，配备， 使智力或者体力上有准备的
+// climbe // 爬 去c 是limb 是有手 枝的意思
+// frowm//皱眉
+// browm // 棕色
+
+
+
+
+
+
+
+sincere //真诚 真挚的
+lad // 男孩 小伙子
+frowm // 
+equip //准备
+//civilization 文化 名词
+// statistic   统计
+
+
+
+
+array的方法需要好好看看
