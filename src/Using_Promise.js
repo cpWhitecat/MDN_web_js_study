154633f2983f52795878048611fb8c41cdfbcb5b
cpWhitecat <19803cp@gmial.com>
2022-04-09 16:51:34 +0800

    Promise的时序

--- src/Using_Promise.js
+++ src/Using_Promise.js
@@ -1,3 +1,5 @@
+const { resolve } = require("path")
+
 Promise是一个异步(Async) --- 且只会执行一次
 
 
@@ -6,10 +8,35 @@ Promise是一个异步(Async) --- 且只会执行一次
 如果then前面的链式调用返回的是一个value,那就会使用这个value调用下一个<then>
 如果then前面返回的是一个<Promise>对象,那后面的<then>会等返回的Promise执行完再调用
 
-let myPromise  = new Promise
-Promise.then(myPromise).then
+let myPromise  = new Promise()   //括号里的函数并不会异步 而是立即执行
+Promise.then(myPromise).then()
+catch的错误捕捉,只能捕捉同级链上的错误,在其他链上的捕捉不到
 
 
 
+
+
+Promise的时序问题
+//如之后还有不懂 可点击
+//https://segmentfault.com/q/1010000020368538
+
+
+var myPromise1 = new Promise((resolve)=>{
+    console.log('myPromise1');
+    resolve();  //resolve一定要传参，不然后面的then就不会执行
+}).then(()=>{
+    console.log("then外部1");
+    new Promise(resolve=>{
+        console.log("内部Promise");
+        resolve();
+    }).then(()=>{
+        console.log("then内部1");
+    }).then(()=>{
+        console.log("then内部2");
+    })
+}).then(()=>{
+    console.log("外部then2");
+})
+myPromise1()
 throw//是用来抛出用户定义的错误\
 
