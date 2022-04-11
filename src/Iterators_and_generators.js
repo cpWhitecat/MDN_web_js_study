5b30438bf34a7a4e627fe0829c501ec93e073771
cpWhitecat <19803cp@gmial.com>
2022-04-11 22:41:42 +0800

    Reflect

--- src/Iterators_and_generators.js
+++ src/Iterators_and_generators.js
@@ -20,3 +20,30 @@ handler.construct()
 let p  = new Proxy  // 直接new Proxy 会报TypeError
 所以这时候需要________construct()
 
+
+
+handler.apply()
+// 拦截函数调用  
+// 拦截 function.apply  function.call()  Reflect.apply()    关于Reflect对象等后来读文档
+
+handler还有很多方法未使用了解   //  待去看Vue源码 再逐一挖深了解
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Reflect对象  // 它并不能想Proxy一样使用new  它不是一个构造函数
+let _Reflect = new Reflect   // TypeError
