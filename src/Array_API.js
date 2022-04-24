3a5581b7411af803f79fe4f77fa820b0e2eaff8e
cpWhitecat <19803cp@gmail.com>
2022-04-24 22:32:06 +0800

    array

---
+++ src/Array_API.js
@@ -0,0 +1,15 @@
+Array.prototype.filter()
+
+
+Array.prototype.reduce()
+
+
+
+
+Array.prototype.find()
+
+/* 对于find()函数在一个数组中找值
+它找的是当时那个数组索引值所在的状态
+它找的对应的值是根据当时索引对应现在的值 */
+// 这个方法感觉可以使用回调函数函数
+// 固定找某个值，虽然没什么效率但是个想法
