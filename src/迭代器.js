c0e9992ac4fa28ea58d6810597d519758307c63c
cpWhitecat <19803cp@gmail.com>
2022-06-27 23:29:59 +0800

    for of and for in

---
+++ src/迭代器.js
@@ -0,0 +1,11 @@
+for (const iterator of object) {
+    
+}
+
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
